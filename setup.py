5cdf8c9eb545ecd859c60465ad35cc0d0eede7b4
Zach Evans <zqevans@gmail.com>
2024-06-03 18:08:09 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stable-audio-tools-dev',
-    version='0.0.11',
-    url='https://github.com/Stability-AI/stable-audio-tools-dev.git',
+    name='stable-audio-tools',
+    version='0.0.16',
+    url='https://github.com/Stability-AI/stable-audio-tools.git',
     author='Stability AI',
     description='Training and inference tools for generative audio models from Stability AI',
     packages=find_packages(),  
