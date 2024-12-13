e8e71cf20d31396accc91ec6ee83f80bb08df02f
youngmae <youngmae>
2024-12-13 15:14:41 -0800

    add 8bit optimizer support & documentation

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
         'aeiou==0.0.20',
         'alias-free-torch==0.0.6',
         'auraloss==0.4.0',
+        'bitsandbytes==0.35.0',
         'descript-audio-codec==1.0.0',
         'einops==0.7.0',
         'einops-exts==0.0.4',
