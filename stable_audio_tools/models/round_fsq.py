dfff8904e663a6deb37bda1263a84e597e4bf188
Fei Gao <feigao@tik42x.ee.ethz.ch>
2025-07-06 19:26:18 +0200

    rounding latent, big change

---
+++ stable_audio_tools/models/round_fsq.py
@@ -0,0 +1,44 @@
+"""
+Dithered Finite Scalar Quantization
+Code adapted from https://github.com/lucidrains/vector-quantize-pytorch/blob/master/vector_quantize_pytorch/finite_scalar_quantization.py
+"""
+
+from typing import List, Tuple
+import random
+
+import torch
+import torch.nn as nn
+from torch.nn import Module
+from torch import Tensor, int32
+from torch.amp import autocast
+
+from einops import rearrange
+
+
+def round_ste(z: Tensor) -> Tensor:
+    """Round with straight through gradients."""
+    zhat = z.round()
+    return z + (zhat - z).detach()
+
+class RoundFSQ(Module):
+    def __init__(self, latent_dim: int, dither: bool = False):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.dither = dither
+        # no dither at inference
+        # you control dither at training
+
+    def quantize(self, z):
+        if self.training and self.dither:
+            z = z + (torch.rand_like(z) - 0.5)
+        quantized = round_ste(z)
+
+        return quantized
+
+    @autocast(device_type="cuda", enabled = False)
+    def forward(self, z, skip_tanh: bool = False):
+        # z: [B, N, T]
+        assert z.shape[-1] == self.dim, f"Expected last dim={self.dim}, got {z.shape[-1]}"
+        q = self.quantize(z)
+        indices = q.to(torch.int64)
+        return q, indices
